47f8b3860b6581cc6a12b9f9f45ff2a03dfe59b2
Howard Yeh <hayeah@gmail.com>
2011-10-21 21:39:10 -0700

    autocompelte tweak

--- modes/_auto-complete.el
+++ modes/_auto-complete.el
@@ -1,7 +1,10 @@
 (add-to-list 'load-path "~/el/lib/auto-complete/")
 (require 'auto-complete)
 
+(setq ac-ignore-case nil)
+(setq ac-auto-show-menu t) ; no delay showing th emenu
 (global-auto-complete-mode)
+
 
 ;; C-s to refine search
 ;; C-g to cancel search
